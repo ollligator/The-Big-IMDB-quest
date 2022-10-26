1501d261505012209a9ce93c7f55ebedd762c5f8
ollligator <shnurocheck@gmail.com>
2022-10-26 15:23:59 +0200

    movie list >> df, added oscar calculator and penalizer

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pandas as pd
-import re
 from bs4 import BeautifulSoup as bs
 import requests
-import csv
 
 
 def get_content(url, page_type):
@@ -25,84 +23,101 @@ def get_content(url, page_type):
 
 
 class Movie:
-    def __init__(self, movie, i):
+    def __init__(self, movies, ind):
         
-        self.i = i
         self.movie_id = []
         self.title = []
         self.rating = 0
         self.votes = 0
         self.oscars = 0
         
-        self.set_id(movie)
-        self.set_title(movie)
-        self.set_rating(movie)
-        self.set_votes(movie)
-        self.set_oscars(movie)
+        self.set_id(movies, ind)
+        self.set_title(movies, ind)
+        self.set_rating(movies, ind)
+        self.set_votes(movies, ind)
+        self.set_oscars()
         
 
-    def set_title(self, movie):
+    def set_title(self, movies, ind):
         len_year = 7
-        title_full = (' '.join(movie.select('.titleColumn')[self.i].get_text().strip().split()).replace('.', ''))
-        self.title = title_full[len(str(self.i))+1:-len_year]
+        title_full = (' '.join(movies.select('.titleColumn')[ind].get_text().strip().split()).replace('.', ''))
+        self.title = title_full[len(str(ind))+1:-len_year]
     
-    def set_rating(self, movie):
-        self.rating = float(movie.select('.ratingColumn.imdbRating')[self.i].get_text().strip())
+    def set_rating(self, movies, ind):
+        self.rating = float(movies.select('.ratingColumn.imdbRating')[ind].get_text().strip())
     
-    def set_votes(self, movie):
-        self.votes = int(movie.select('.posterColumn span[name=nv]')[self.i].get('data-value'))
+    def set_votes(self, movies, ind):
+        self.votes = int(movies.select('.posterColumn span[name=nv]')[ind].get('data-value'))
     
-    def set_id(self, movie):        
-        self.movie_id = movie.find_all(class_='seen-widget')[self.i].get('data-titleid')
+    def set_id(self, movies, ind):        
+        self.movie_id = movies.find_all(class_='seen-widget')[ind].get('data-titleid')
     
-    def set_oscars(self, movie):
-        
-        oscars = 0;
+    def set_oscars(self):
         award_url = 'https://www.imdb.com/title/' + self.movie_id +'/awards/?ref_=tt_awd/'
         award_content = get_content(award_url, 'lxml')
         for award in award_content.select('.awards'):
             aw = award.find(class_='title_award_outcome').get_text()
             if ("Winner" in aw) and ("Oscar" in aw):
-                oscars = award.find(class_='title_award_outcome').get('rowspan')        
-        self.oscars = oscars
+                self.oscars = int(award.find(class_='title_award_outcome').get('rowspan'))        
+        
+        
 
-class Scraper:
+class IMDBDataManager:
     def __init__(self, num_movies):
         self.num_movies = num_movies
         self.movie_list = []
+        self.df_movies = pd.DataFrame()
         
-    def get_data(self, url):
+    def Scraper(self, url):
+        cols = ['Title', 'Rating', 'Votes', 'Oscars']
         content = get_content(url, 'lxml')
-        for movie in content.select('.lister-list'):
+        for movies in content.select('.lister-list'):
             for i in range(self.num_movies):
-                mov = Movie(movie, i)
+                mov = Movie(movies, i)
+                data = [mov.title, 
+                        mov.rating,
+                        mov.votes,
+                        mov.oscars]
                 self.movie_list.append(mov)
-            
-    def create_csv(self):
-        with open('top250.csv', 'w',) as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['Title', 'Rating', 'Votes', 'Oscars'])
-            for movie in self.movie_list:
-                writer.writerow([movie.title, 
-                                 movie.rating,
-                                 movie.votes,
-                                 movie.oscars]) 
+                self.df_movies = self.df_movies.append(pd.Series(data, index=cols),ignore_index=True)
         
-def ReviewPenalizer(movie_list):
-    max_votes = max([movie.votes for movie in movie_list])
-    penalty_step = 100000
-    deduction = 0.1
-    for movie in movie_list:
-        penalty = (max_votes - movie.votes) // penalty_step
-        print(penalty* deduction)
+        self.df_movies.to_csv('IMDBtop20_initial.csv')
             
-num_movies = 10     
+        
+    def ReviewPenalizer(self):
+        penalty_step = 100000
+        deduction = 0.1
+        
+        max_votes = max([movie.votes for movie in self.movie_list])
+        max_votes = self.df_movies['Votes'].max()    
+             
+        self.df_movies['Rating_Review_Correction'] = round((max_votes - self.df_movies['Votes']) // penalty_step * deduction,3)
+        self.df_movies['Rating_Review'] =  round(self.df_movies['Rating'] - self.df_movies['Rating_Review_Correction'],3) 
+            
+    def OscarCalculator(self):
+        oscar_mapper = {0: 0, range(1,3): 0.3, range(3,6): 0.5, range(6,11): 1, range(11,100): 1.5}
+        self.df_movies['Rating_Oscar_Correction']= round(self.df_movies['Oscars'].replace(oscar_mapper),3)
+        self.df_movies['Rating_Oscar'] =  round(self.df_movies['Rating'] + self.df_movies['Rating_Oscar_Correction'],3)
+        
+    
+    def StoreNewRatings(self):
+        if(self.df_movies.shape[1]>6):
+            self.df_movies['Rating_New'] = round(self.df_movies['Rating'] + self.df_movies['Rating_Oscar_Correction'] - self.df_movies['Rating_Review_Correction'],3)
+            sorted_df = self.df_movies.sort_values(by=['Rating_New'], ascending=False, ignore_index=True)
+            sorted_df.to_csv('IMDBtop20_newrating.csv')
+            
+
+
+            
+num_movies = 20     
 top_url = 'https://www.imdb.com/chart/top/'
 
-scraper = Scraper(num_movies)
-scraper.get_data(top_url)
-#scraper.create_csv()
-ReviewPenalizer(scraper.movie_list)
+manager  = IMDBDataManager(num_movies)
+manager.Scraper(top_url)
+#manager.create_csv()
+manager.ReviewPenalizer()
+manager.OscarCalculator()
+manager.StoreNewRatings()
 
         
 
