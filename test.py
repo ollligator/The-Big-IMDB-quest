80942558e8b13f1e535cceaa8e7cdf8a36fa9e9b
ollligator <shnurocheck@gmail.com>
2022-10-27 23:06:17 +0200

    3 tests added

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ from main import IMDBDataManager
 # This is the class we want to test. So, we need to import it
 class TestIMDBDataManager(unittest.TestCase):
     def setUp(self):
-        self.num_movies = 2
-
+        self.num_movies = 20
     def test_scraper_data_correctness(self):
         url = 'https://www.imdb.com/chart/top/'
         manager  = IMDBDataManager(self.num_movies)
@@ -40,6 +39,35 @@ class TestIMDBDataManager(unittest.TestCase):
         for i in range(self.num_movies):
             self.assertGreaterEqual(manager.df_movies.loc[i, 'Rating_Oscar_Correction'],0)
             self.assertLessEqual(manager.df_movies.loc[i, 'Rating_Oscar_Correction'],1.5)
+            
+    def test_penalizer_min_max(self):       
+        url = 'https://www.imdb.com/chart/top/'
+        manager  = IMDBDataManager(self.num_movies)
+        manager.Scraper(url)   
+        manager.df_movies.loc[0, 'Votes'] = 0
+        manager.df_movies.loc[self.num_movies-1, 'Votes'] = 100000000
+        manager.ReviewPenalizer()
+        self.assertEqual(manager.df_movies['Rating_Review_Correction'][0], 0.1*100000000 // 100000)
+        
+    def test_penalizer_rating_ranges(self):
+        url = 'https://www.imdb.com/chart/top/'
+        manager  = IMDBDataManager(self.num_movies)
+        manager.Scraper(url)
+        manager.ReviewPenalizer()
+        
+        for i in range(self.num_movies):
+            self.assertGreaterEqual(manager.df_movies.loc[i, 'Rating_Review_Correction'],0)
+            self.assertLessEqual(manager.df_movies.loc[i, 'Rating_Review_Correction'],10)
+            
+    def test_sorted_final_rating(self):
+        url = 'https://www.imdb.com/chart/top/'
+        manager  = IMDBDataManager(self.num_movies)
+        manager.Scraper(url)
+        manager.OscarCalculator()
+        manager.ReviewPenalizer()
+        manager.StoreNewRatings()
+        self.assertGreaterEqual(manager.df_movies.loc[0, 'Rating_New'],manager.df_movies.loc[self.num_movies-1, 'Rating_New'])
+        
             
     
         
